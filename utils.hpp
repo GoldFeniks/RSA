615d708e502842d642fb06dd6e46a243a838c8d1
Tyshchenko Andrey <ggoldenfeniks@gmail.com>
2018-11-02 12:00:50 +1000

    Use boost::multiprecision::miller_rabin_test

--- utils.hpp
+++ utils.hpp
@@ -10,6 +10,7 @@
 #include <boost/math/special_functions/prime.hpp>
 #include <boost/random/mersenne_twister.hpp>
 #include <boost/random/uniform_int_distribution.hpp>
+#include <boost/multiprecision/miller_rabin.hpp>
 
 namespace mp = boost::multiprecision;
 namespace math = boost::math;
@@ -45,42 +46,18 @@ namespace rsa {
 
         }
 
-        static auto generate_random_prime(const size_t k = 100) {
+        static auto generate_random_prime(const unsigned int k = 100) {
             const auto rand = get_int_random();
             while(true) {
                 const auto n = rand();
                 for (unsigned int i = 0; i < math::max_prime; ++i)
                     if (n % math::prime(i) == 0)
                         goto next;
-                if (miller_rabin_test(n, k))
+                if (mp::miller_rabin_test(n, k))
                     return n;
                 next:
                 continue;
             }
-        }
-
-        static auto miller_rabin_test(const number& n, const size_t k) {
-            const auto rand = get_int_random(2, n - 2);
-            auto d = n - 1;
-            size_t r = 0;
-            while (d % 2 == 0) {
-                d /= 2;
-                ++r;
-            }
-            for (size_t i = 0; i < k; ++i) {
-                auto x = mp::powm(rand(), d, n);
-                if (x == 1 || x == n - 1)
-                    continue;
-                for (size_t j = 0; j < r - 1; ++j) {
-                    x = mp::powm(x, 2, n);
-                    if (x == n - 1)
-                        goto next;
-                }
-                return false;
-                next:
-                continue;
-            }
-            return true;
         }
 
         static auto bezout_identity(const signed_number& a, const signed_number& b) {
